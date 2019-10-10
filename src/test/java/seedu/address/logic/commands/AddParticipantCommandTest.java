7905abd04ad43fe6063ea7d43f8d6c7a6178ca4b
john0227 <37623568+john0227@users.noreply.github.com>
2019-10-10 22:17:35 +0800

    Implemented test for AddParticipantCommand and StringUtil class

---
+++ src/test/java/seedu/address/logic/commands/AddParticipantCommandTest.java
@@ -0,0 +1,46 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.addcommand.AddParticipantCommand;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.entity.Id;
+import seedu.address.model.entity.Participant;
+import seedu.address.model.entity.PrefixType;
+import seedu.address.stub.ModelManagerStub;
+import seedu.address.testutil.Assert;
+import seedu.address.testutil.ParticipantBuilder;
+
+public class AddParticipantCommandTest {
+
+    @Test
+    public void constructor_nullPerson_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new AddParticipantCommand(null));
+    }
+
+    @Test
+    public void execute_personAcceptedByModel_addSuccessful() throws Exception {
+        ModelManagerStub modelStub = new ModelManagerStub();
+        Participant validParticipant = new ParticipantBuilder().build();
+
+        CommandResult commandResult = new AddParticipantCommand(validParticipant).execute(modelStub);
+
+        assertEquals(String.format(AddParticipantCommand.MESSAGE_SUCCESS, validParticipant),
+                commandResult.getFeedbackToUser());
+        assertEquals(validParticipant, modelStub.getParticipant(new Id(PrefixType.P, 1)));
+    }
+
+    @Test
+    public void execute_duplicatePerson_throwsCommandException() {
+        Participant validParticipant = new ParticipantBuilder().build();
+        AddParticipantCommand addParticipantCommand = new AddParticipantCommand(validParticipant);
+        ModelManagerStub modelStub = new ModelManagerStub();
+
+        Assert.assertThrows(CommandException.class, AddParticipantCommand.MESSAGE_DUPLICATE_PARTICIPANT,
+                () -> addParticipantCommand.execute(modelStub));
+    }
+
+}
