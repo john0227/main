936c80f1a66742640c2b593c334ce626be0515c2
john0227 <37623568+john0227@users.noreply.github.com>
2019-10-18 12:31:00 +0800

    Fixed checkstyle issue

--- src/main/java/seedu/address/model/entity/SubjectName.java
+++ src/main/java/seedu/address/model/entity/SubjectName.java
@@ -1,7 +1,6 @@
 package seedu.address.model.entity;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Encapsulates possible topics for a Hackathon project.
